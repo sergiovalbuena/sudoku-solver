4c5b474f75c18944a65f479199960bbae71b137a
sergiovalbuena <iosergio.valbuena@gmail.com>
2024-02-20 21:32:56 -0800

    Add initial files and code for Sudoku solver

---
+++ app.js
@@ -0,0 +1,75 @@
+const puzzleBoard = document.querySelector("#puzzle");
+const solveButton = document.querySelector("#solve-button");
+const solutionDisplay = document.querySelector("#solution");
+const squares = 81;
+const submission = [];
+
+for (let i = 0; i < squares; i++) {
+  const square = document.createElement("input");
+  square.setAttribute("type", "number");
+  square.setAttribute("min", "1");
+  square.setAttribute("max", "9");
+
+  let row = Math.floor(i / 9);
+  let col = i % 9;
+
+  if ((Math.floor(row / 3) + Math.floor(col / 3)) % 2 === 0) {
+    square.classList.add("odd-section");
+  }
+
+  puzzleBoard.appendChild(square);
+}
+
+const joinValues = () => {
+  const inputs = document.querySelectorAll("input");
+  inputs.forEach((input) => {
+    if (input.value) submission.push(input.value);
+    else submission.push(".");
+  });
+  console.log(submission);
+};
+//solveButton.addEventListener("click", joinValues);
+
+const populateValues = (isSolvable, solution) => {
+  const inputs = document.querySelectorAll("input");
+  if (isSolvable && solution) {
+    inputs.forEach((input, i) => {
+      input.value = solution[i];
+    });
+    solutionDisplay.textContent = "Solved!";
+  } else {
+    solutionDisplay.textContent = "Unsolvable!";
+  }
+};
+
+const solve = async () => {
+  joinValues();
+  const data = submission.join("");
+  console.log("data", data);
+
+  const options = {
+    method: "POST",
+    url: "https://solve-sudoku.p.rapidapi.com/",
+    headers: {
+      "content-type": process.env.CONTENT_TYPE,
+      "X-RapidAPI-Key": process.env.X_RAPIDAPI_KEY,
+      "X-RapidAPI-Host": process.env.X_RAPIDAPI_HOST,
+    },
+    data: {
+      puzzle:
+        "2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3",
+    },
+  };
+
+  axios
+    .request(options)
+    .then((response) => {
+      console.log(response.data);
+      populateValues(response.data.solvable, response.data.solution);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+solveButton.addEventListener("click", solve);
